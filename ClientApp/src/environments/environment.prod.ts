2130121fffdb7240cda8a8a31fe7ed61e10aa559
alexandrustf <stefanalexandruioan@yahoo.com>
2020-08-26 13:58:40 +0300

    added googgle maps api KEY

--- ClientApp/src/environments/environment.prod.ts
+++ ClientApp/src/environments/environment.prod.ts
@@ -1,7 +1,9 @@
 export const environment = {
   production: true,
   YourApartmentUrl:
-    "https://yourapartmentwebapi2-20200613035211.azurewebsites.net/api/v1",
+    "https://7zjkc0nbtk.execute-api.eu-central-1.amazonaws.com/development",
+  LocationsUrl: "http://localhost:8080/api/v1",
+
   ApartmentMLUrl: "https://deployapartment.nw.r.appspot.com",
 
 };
