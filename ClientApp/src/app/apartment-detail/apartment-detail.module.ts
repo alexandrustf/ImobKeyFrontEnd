47ccd8b668f1d28de52f8044ccb3fb097a1cae47
alexandrustf <stefanalexandruioan@yahoo.com>
2021-01-10 22:06:01 +0200

    carousel

--- ClientApp/src/app/apartment-detail/apartment-detail.module.ts
+++ ClientApp/src/app/apartment-detail/apartment-detail.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from "@angular/common";
 import { ApartmentDetailComponent } from "./apartment-detail/apartment-detail.component";
 import { SpinnerModule } from "../spinner/spinner.module";
 import { AgmCoreModule } from "@agm/core";
+import { MatCarouselModule } from '@ngmodule/material-carousel';
 
 @NgModule({
   declarations: [ApartmentDetailComponent],
   imports: [CommonModule, SpinnerModule, AgmCoreModule.forRoot({
     apiKey: "AIzaSyBVmIevOIYNBX1c4ZKEcNArXL04WS_JVO0",
-  }),],
+  }),
+  MatCarouselModule.forRoot()],
 })
 export class ApartmentDetailModule {}
